d616fb04e7b1cf7a471c0ae86645ab517634517c
kinbei-math <syomaonepiece@gmail.com>
2026-02-18 20:34:44 +0900

    chore: initial Spring Boot project setup

---
+++ src/main/java/com/example/todo_api_v2/TodoApiV2Application.java
@@ -0,0 +1,13 @@
+package com.example.todo_api_v2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodoApiV2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TodoApiV2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/todo_api_v2/TodoApiV2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.todo_api_v2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodoApiV2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
