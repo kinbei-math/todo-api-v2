f88d26cee66577a587555e210ae9565849259c96
kinbei-math <syomaonepiece@gmail.com>
2026-05-01 23:19:40 +0900

    feat(item): ItemControllerを追加（GET/POST、SecurityConfigも修正）

--- src/main/java/com/example/todo_api_v2/config/SecurityConfig.java
+++ src/main/java/com/example/todo_api_v2/config/SecurityConfig.java
@@ -12,8 +12,8 @@ import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.context.annotation.Profile;
 import org.springframework.core.annotation.Order;
 
-@Configuration//設定ファイルすべてにつける
-@EnableWebSecurity//WebSecurityを有効にする
+@Configuration// 設定ファイルすべてにつける
+@EnableWebSecurity// WebSecurityを有効にする
 public class SecurityConfig {
 
     //Securityマニュアルの詳細(本番環境用)
@@ -23,8 +23,8 @@ public class SecurityConfig {
         //csrfを無効化
         http.csrf(csrf -> csrf.disable());
 
-        //DELETEのみADMINだけに認可。
-        //残りのメソッドはすべてログイン済みユーザーに認可。
+        // DELETEのみADMINだけに認可。
+        // 残りのメソッドはすべてログイン済みユーザーに認可。
         http.authorizeHttpRequests(authz ->
                 authz.requestMatchers(HttpMethod.DELETE,"/todos/**")
                         .hasRole("ADMIN")
@@ -32,10 +32,10 @@ public class SecurityConfig {
                         .authenticated()
         );
 
-        //Basic認証の有効化
+        // Basic認証の有効化
         http.httpBasic(Customizer.withDefaults());
 
-        //詳細を記載(完成)したマニュアルを返す
+        // 詳細を記載(完成)したマニュアルを返す
         return http.build();
     }
 
@@ -52,7 +52,7 @@ public class SecurityConfig {
         return http.build();
     }
 
-    //パスワードをハッシュ化(暗号化)
+    // パスワードをハッシュ化(暗号化)
     @Bean
     public PasswordEncoder passwordEncoder(){
         return new BCryptPasswordEncoder();
---
+++ src/main/java/com/example/todo_api_v2/controller/ItemController.java
@@ -0,0 +1,42 @@
+package com.example.todo_api_v2.controller;
+
+import com.example.todo_api_v2.dto.ItemCreateRequest;
+import com.example.todo_api_v2.dto.ItemResponse;
+import com.example.todo_api_v2.service.ItemService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/items")
+public class ItemController {
+
+    private final ItemService itemService;
+
+    public ItemController(ItemService itemService) {
+        this.itemService = itemService;
+    }
+
+    // 1. GET byId
+    @GetMapping("/{id}")
+    public ResponseEntity<ItemResponse> getItem(@PathVariable Long id) {
+        return ResponseEntity.status(HttpStatus.OK).body(itemService.findById(id));
+    }
+
+    // 2. GET All
+    @GetMapping
+    public ResponseEntity<List<ItemResponse>> getItems() {
+        return ResponseEntity.status(HttpStatus.OK).body(itemService.findAll());
+    }
+
+    // 3. POST insert
+    @PostMapping
+    public ResponseEntity<ItemResponse> createItem(
+            @Validated @RequestBody ItemCreateRequest itemCreateRequest) {
+        return  ResponseEntity.status(HttpStatus.CREATED).body(itemService.createItem(itemCreateRequest));
+
+    }
+}
