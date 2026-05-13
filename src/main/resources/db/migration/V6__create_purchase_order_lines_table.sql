c6b69a838fc6cc29a6bdbcd438045bf4eff7bbcb
kinbei-math <syomaonepiece@gmail.com>
2026-05-13 19:22:57 +0900

    fix(w16): received_atをDATE型に変更(Business Time扱いに統一)

--- src/main/resources/db/migration/V6__create_purchase_order_lines_table.sql
+++ src/main/resources/db/migration/V6__create_purchase_order_lines_table.sql
@@ -6,14 +6,14 @@ CREATE TABLE purchase_order_lines(
     line_no     SMALLINT      NOT NULL,       -- 行番号
     qty         DECIMAL(12,3) NOT NULL,       -- 発注数量
     price       DECIMAL(12,2) NOT NULL,       -- 単価
-    due_date    DATE          NOT NULL,       -- 納期
+    due_date    DATE          NOT NULL,       -- 納期(Business Time)
     status      VARCHAR(20)   NOT NULL,       -- enum:{ORDERED, RECEIVED} SQLでは文字列で受け取る
     received_by VARCHAR(255)  DEFAULT NULL,   -- receivedに変更させた人
-    received_at TIMESTAMP     DEFAULT NULL,   -- receivedに変更した時間
+    received_at DATE          DEFAULT NULL,   -- 納品日(Business Time)
     created_by  VARCHAR(255)  NOT NULL,       -- 登録者
-    created_at  TIMESTAMP     NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    created_at  TIMESTAMP     NOT NULL DEFAULT CURRENT_TIMESTAMP, -- System Time
     updated_by  VARCHAR(255)  DEFAULT NULL,
-    updated_at  TIMESTAMP     NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    updated_at  TIMESTAMP     NOT NULL DEFAULT CURRENT_TIMESTAMP, -- System Time
 
     CONSTRAINT pk_purchase_order_lines_id                  PRIMARY KEY (id),
     CONSTRAINT fk_purchase_order_lines_po_id           FOREIGN KEY (po_id)
