a3c3e385dcaecefe5ed728e345c7350e44caae37
kinbei-math <syomaonepiece@gmail.com>
2026-05-13 21:44:54 +0900

    fix(w16): purchase_ordersにupdated_byカラム追加(ヘッダ監査の一貫性)

--- src/main/resources/db/migration/V5__create_purchase_orders_table.sql
+++ src/main/resources/db/migration/V5__create_purchase_orders_table.sql
@@ -5,8 +5,9 @@ CREATE TABLE purchase_orders(
     supplier    VARCHAR(100) NOT NULL,                           -- 仕入れ先
     status      VARCHAR(20)  NOT NULL,                           -- enum:{ORDERED, RECEIVED} SQLでは文字列で受け取る
     created_at  TIMESTAMP    NOT NULL DEFAULT CURRENT_TIMESTAMP, -- System Time
-    updated_at  TIMESTAMP    NOT NULL DEFAULT CURRENT_TIMESTAMP, -- 初期値はDB登録時と同じ
     created_by  VARCHAR(255) NOT NULL,                           -- 作成者
+    updated_at  TIMESTAMP    NOT NULL DEFAULT CURRENT_TIMESTAMP, -- 初期値はDB登録時と同じ(Bisiness Time)
+    updated_by  VARCHAR(255) DEFAULT NULL,                       -- 更新者
 
     CONSTRAINT pk_purchase_orders_id        PRIMARY KEY (id),
     CONSTRAINT uk_purchase_orders_po_number UNIQUE(po_number)
