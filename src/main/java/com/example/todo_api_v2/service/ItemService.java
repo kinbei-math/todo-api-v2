92bfb1117427d28176d233a4e311a0ecb88dd82f
kinbei-math <syomaonepiece@gmail.com>
2026-06-28 21:59:42 +0900

    feat(W17): 発注推奨のService層メソッドを追加

--- src/main/java/com/example/todo_api_v2/service/ItemService.java
+++ src/main/java/com/example/todo_api_v2/service/ItemService.java
@@ -2,6 +2,7 @@ package com.example.todo_api_v2.service;
 
 import com.example.todo_api_v2.dto.item.ItemCreateRequest;
 import com.example.todo_api_v2.dto.item.ItemResponse;
+import com.example.todo_api_v2.dto.item.ReorderAlertResponse;
 import com.example.todo_api_v2.dto.stock.StockResponse;
 import com.example.todo_api_v2.entity.Item;
 import com.example.todo_api_v2.exception.DuplicateItemCodeException;
@@ -72,7 +73,6 @@ public class ItemService {
     }
 
     // 4. 現在庫を返す
-    @Transactional
     public StockResponse getCurrentStock(Long itemId){
         // Itemの取得(itemIdに対しての存在確認)
         Item item = itemMapper.findById(itemId)
@@ -86,6 +86,10 @@ public class ItemService {
         return convertStockResponse(item, currentStock);
     }
 
+    // 5. 発注点を切ったものを返す
+    public List<ReorderAlertResponse> reorderItems() {
+        return itemMapper.reorderItems();
+    }
 
     // Responseに移し替えるメソッド 2種
     private ItemResponse convertItemResponse(Item item){
